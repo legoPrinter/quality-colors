d67a45d64064e4c28ca04211200ce3f853e67be0
legoPrinter <79933626+legoPrinter@users.noreply.github.com>
2024-08-12 14:19:14 -0400

    improved storing qualities

---
+++ src/main.py
@@ -0,0 +1,80 @@
+import json;
+from termcolor import cprint
+
+import user_reader
+
+NUM_SELECTED_COLORS = 5
+
+COLORS = {
+    "red": [255, 0, 0],
+    "yellow": [255,255,0],
+    "green": [0, 255, 0],
+    "blue":  [0, 0, 255],
+    "pink": [255,105,180],
+    "orange": [255,165,0],
+    "violete": [153,50,204],
+    "brown": [139,69,19],
+    "black": [0,0,0],
+    "white": [245,245,245],
+    "gray": [170,170,170]
+}
+
+color_qualities = json.load(open("../res/color_qualities.json"))
+
+def find_color_weights(user_qualities):
+    # Color_qualities and it's children are dictionaries
+    # Iterating over dictionaries provides their keys
+
+    color_weights = {}
+
+    for color_name in color_qualities:
+        differences = 0
+        color = color_qualities[color_name]
+        for quality in color:
+            difference = abs(color[quality] - user_qualities[quality])
+            differences += difference
+
+        # find the weight of the color. The smaller the difference, the bigger the weight
+        color_weight = 1 - ((float(differences) / float(qualities_length)) / 4.0)
+
+        color_weights[color_name] = color_weight
+
+    return color_weights
+
+
+def find_top_n_weight_keys(value_set, n):
+    result = []
+    for _count in range(0, n):
+        max_value = -1
+        max_key = 0
+        for key in value_set:
+            value = value_set[key]
+            if value > max_value:
+                max_value = value
+                max_key = key
+        result.append(max_key)
+        value_set.pop(max_key)
+    return result
+
+
+# 1. record the user's traits & values
+user_reader.get_user_qualities()
+
+# 2. give weights to the colors based on how relevant they are to the user's qualities
+#color_weights = find_color_weights(user_qualities)
+
+# 3. pick the N most relavant colors
+#top_color_names = find_top_n_weight_keys(color_weights, NUM_SELECTED_COLORS)
+
+# 4. convert the color names to rgb values
+#top_colors_rgb = []
+#for color_name in top_color_names:
+#    top_colors_rgb.append(COLORS[color_name])
+
+# 5. display the colors
+
+#print("| these are your colors from most to least significant: \n")
+
+#for i in range(0, NUM_SELECTED_COLORS):
+#    color = top_colors_rgb[i]
+#   cprint("\033[38;2;" + str(color[0]) + ";" + str(color[1]) + ";" + str(color[2]) + "m| - " + top_color_names[i] + "\033[0m")
---
+++ src/user_reader.py
@@ -0,0 +1,210 @@
+import json
+
+common_qualities = json.load(open("../res/common_qualities.json"))
+
+example_user_qualities = {
+    "core values": ["honesty", "integrity", "compassion", "loyalty", "respect"],
+    "sources of pride": ["painting", "running", "puzzles", "card collecting", "career"],
+    "personality traits": {
+        "openness": 1,
+        "conscientiousness": 5,
+        "extraversion": 2,
+        "agreeableness": 4,
+        "neuroticism": 2
+    }
+}
+
+def print_list_ordered(items):
+    items_length = len(items)
+    index = 0
+
+    while index < items_length:
+        item = items[index]
+        index += 1
+        print(f"{index}. {item}")
+
+def get_user_choises(items, amount_choises, key_word):
+    items_length = len(items)
+
+    if items_length < amount_choises:
+        raise Exception("Number of choises exceeds list length")
+
+    choises_left = amount_choises
+
+    choises = []
+
+    while choises_left != 0:
+        print_list_ordered(items)
+        if choises_left == amount_choises:
+            print(f"choose {amount_choises} {key_word} from the list: ")
+        else:
+            choise_or_choises = "choises"
+            if choises_left == 1: 
+                "choise"
+
+            print(f"you have {choises_left} {choise_or_choises} left: ")
+
+        # get user input
+        choise = input()
+
+        # try to parse the user input
+        try:
+            choise = int(choise)
+        except ValueError:
+            print("please enter a number")
+            continue
+
+        # check if the input is in bounds
+        if choise < 1 or choise > items_length:
+            print("please choose a number from one of the lables")
+            continue
+
+        # at this point, the choise is validated. Decrement the choises left
+        choises_left -= 1
+
+        # print the user's choise
+        print(f"your choise was: #{choise}. {items[choise-1]}")
+
+        # remove the user's choise from the list
+        del items[choise-1]
+        items_length -= 1
+
+        # add the user's choise to the list of choises
+        choises.append(choise-1)
+
+    return choises
+
+def get_user_choises_from_categories(categories, amount_choises, key_word):
+    choises_left = amount_choises
+
+    choises = []
+
+    print(f"choose {amount_choises} {key_word} from the list.")
+
+    while choises_left != 0:
+        # print the category names
+        category_names = list(categories.keys())
+        print_list_ordered(category_names)
+
+        # get the users category
+        print("pick a category to view it: ")
+        choise = input()
+
+        # try to parse the user input
+        try:
+            choise = int(choise)
+        except ValueError:
+            print("please enter a number")
+            continue
+
+        # check if the input is in bounds
+        if choise < 1 or choise > len(category_names):
+            print("please choose a number from one of the lables")
+            continue
+
+        chosen_category_name = category_names[choise-1]
+        category_items = categories[chosen_category_name]
+        category_items_length = len(category_items)
+
+        while True:
+
+            # display the list in the category
+            print(f"{chosen_category_name}:")
+            print_list_ordered(category_items)
+
+            # print how mutch choises the user has
+            choise_or_choises = "choises"
+            if choises_left == 1: 
+                "choise"
+            print(f"you have {choises_left} {choise_or_choises} left.")
+
+            # get user input
+            print("Enter the number beside your choise, or type \"b\" to go back: ")
+            choise = input()
+
+            # check if the user wants to go back
+            if choise == "b":
+                break
+
+            # try to parse the user input
+            try:
+                choise = int(choise)
+            except ValueError:
+                print("please enter a number")
+                continue
+
+            # check if the input is in bounds
+            if choise < 1 or choise > category_items_length:
+                print("please choose a number from one of the lables")
+
+            # at this point, the choise is validated. Decrement the choises left
+            choises_left -= 1
+
+            # print the user's choise
+            print(f"your choise was: #{choise}. {category_items[choise-1]}")
+
+            # remove the user's choise from the list
+            del categories[chosen_category_name][choise-1]
+
+            # add the user's choise to the list of choises
+            choises.append(choise-1)
+
+            break
+
+    return choises
+
+def get_user_core_values(amount_choises):
+    return get_user_choises(common_qualities["core values"], amount_choises, "of your core values")
+
+def get_user_sources_of_pride(amount_choises):
+    return get_user_choises_from_categories(common_qualities["sources of pride"], amount_choises, "of your sources of pride")
+
+def get_user_personality_traits():
+    personality_traits = common_qualities["personality traits"]
+
+    user_personality_traits = {}
+
+    index = 0
+
+    print("now, rate how much you have these personality traits from 1 - 5 inclusively: ")
+    while index < len(personality_traits):
+
+        # print the trait
+        personality_trait = personality_traits[index]
+        print(f"{index + 1}. {personality_trait}: ")
+
+        # get user input
+        choise = input()
+
+        # try to parse the user input
+        try:
+            choise = int(choise)
+        except ValueError:
+            print("please enter a number")
+            continue
+
+        # check if the input is in bounds
+        if choise < 1 or choise > 5:
+            print("please choose a number from one of the lables")
+            continue
+
+        # increment count
+        index += 1
+        
+        # add the choise to the result
+        user_personality_traits[personality_trait] = choise
+
+    return user_personality_traits
+
+def get_user_qualities():
+    #return example_user_qualities # for debugging purposes
+
+    user_qualities = {
+        "core values": get_user_core_values(3),
+        "sources of pride": get_user_sources_of_pride(3),
+        "personality traits": get_user_personality_traits()
+    }
+
+    print(user_qualities)
+    
+    return user_qualities
